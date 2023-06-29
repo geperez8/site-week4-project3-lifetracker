a9997ece5a5962eeb948b2d91ec02d23cd5647a9
Zyanik <gerardoperez8a@gmail.com>
2023-06-28 22:30:54 -0700

    created and adjusted config.js

---
+++ lifetracker-api/config.js
@@ -0,0 +1,28 @@
+require("dotenv").config()
+require ("colors")
+
+const PORT = process.env.PORT ? Number(process.env.PORT) : 3001
+
+const getDatabaseUri = () => {
+    const dbUser = process.env.DATABASE_USER || "postgres"
+    const dbPass = process.env.DATABASE_PASS  ? encodeURI(process.env.DATABASE_PASS) : "postgres"
+    const dbHost = process.env.DATABASE_HOST || "localhost"
+    const dbPort = process.env.DATABASE_PORT || 5432
+    const dbName = process.env.DATABASE_NAME || "lifetracker"
+
+    // if DATABASE_URL environment variable, use that,
+    // otherwise create the db connection string ourself
+
+    return process.env.DATABASE_URL || `postgresql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`
+
+}
+
+console.log("Vaccine API config".red)
+console.log("PORT:".blue, PORT)
+console.log("Database URI:".blue, getDatabaseUri())
+console.log("-----")
+
+module.exports = {
+    PORT,
+    getDatabaseUri
+}
